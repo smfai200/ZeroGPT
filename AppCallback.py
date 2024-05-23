4711f5a0678e30cc72e801c68c4c841085fff2af
Syed Faizan <smfai200@gmail.com>
2024-05-24 00:58:09 +0500

    Updated with Space

--- AppCallback.py
+++ AppCallback.py
@@ -45,12 +45,12 @@ class AppCallback:
     def __init__(self, app):
         self.app = app
         logging.set_verbosity_error()
-        absolute_path = os.path.dirname(__file__)
-        relative_path = "./roberta-base-openai-detector/"
-        full_path = os.path.join(absolute_path, relative_path)
-        tokenizer = AutoTokenizer.from_pretrained(full_path, local_files_only=True)
-        model = AutoModelForSequenceClassification.from_pretrained(full_path, local_files_only=True)
-        self.classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+        # absolute_path = os.path.dirname(__file__)
+        # relative_path = "./roberta-base-openai-detector/"
+        # full_path = os.path.join(absolute_path, relative_path)
+        # tokenizer = AutoTokenizer.from_pretrained(full_path, local_files_only=True)
+        # model = AutoModelForSequenceClassification.from_pretrained(full_path, local_files_only=True)
+        # self.classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
 
         self.app.callback([
             Output("real_prog", "value"), 
@@ -124,30 +124,30 @@ class AppCallback:
 
     def classifyText(self, text):
         if text != None and text != "":
-            res = self.classifier(text, truncation=True, max_length=510)
-            label = res[0]['label']
-            score = res[0]['score']
+            # res = self.classifier(text, truncation=True, max_length=510)
+            # label = res[0]['label']
+            # score = res[0]['score']
+            #
+            # if label == 'Real':
+            #     real_score = score*100
+            #     fake_score = 100-real_score
+            #     real_score_lable = f"{real_score:.0f}%"
+            #     if int(fake_score) < 10:
+            #         fake_score_lable = ""
+            #     else:
+            #         fake_score_lable =f"{fake_score:.0f}%"
+            # else:
+            #     fake_score = score*100
+            #     real_score = 100-fake_score
+            #     fake_score_lable = f"{fake_score:.0f}%"
+            #     if int(real_score) < 10:
+            #         real_score_lable = ""
+            #     else:
+            #         real_score_lable =f"{real_score:.0f}%"
 
-            if label == 'Real':
-                real_score = score*100
-                fake_score = 100-real_score
-                real_score_lable = f"{real_score:.0f}%"
-                if int(fake_score) < 10:
-                    fake_score_lable = ""
-                else:
-                    fake_score_lable =f"{fake_score:.0f}%"
-            else:
-                fake_score = score*100
-                real_score = 100-fake_score
-                fake_score_lable = f"{fake_score:.0f}%"
-                if int(real_score) < 10:
-                    real_score_lable = ""
-                else:
-                    real_score_lable =f"{real_score:.0f}%"
-
-            # resp = self.recheck_values(text)
-            # return resp
-            return [real_score, real_score_lable, fake_score, fake_score_lable, '']
+            resp = self.recheck_values(text)
+            return resp
+            # return [real_score, real_score_lable, fake_score, fake_score_lable, '']
         else:
             print([50, "", 50, "", ''])
             return [50, "", 50, "", '']
